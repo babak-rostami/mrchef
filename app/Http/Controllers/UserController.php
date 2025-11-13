1db1bf209877c8b5845e2d2a75e1116aeb03b7c5
babak <babakrostami76@gmail.com>
2025-11-13 17:06:32 +0330

    initial Book Store Project

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\EmailService;
+use App\Services\NotifierService;
+use App\Services\SmsService;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+}
---
+++ routes/web.php
@@ -0,0 +1,6 @@
+<?php
+
+use App\Http\Controllers\CarController;
+use App\Http\Controllers\UserController;
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
